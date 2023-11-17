e20a60cd4cb9efb1fde1731199e7a852f4a48b6d
Hieu <hahieu0311@gmail.com>
2023-11-17 10:41:32 +0700

    update

--- client-customer/src/App.js
+++ client-customer/src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './client-customer.css';
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
