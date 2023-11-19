4efb2a7635230baa6af282301b22bb359abcf859
Kevin Stratvert <kevinstratvertyoutube@outlook.com>
2023-11-19 09:57:02 +0700

    updateIndex.js

--- client-customer/src/index.js
+++ client-customer/src/index.js
@@ -6,8 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js";
-import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js";
-import "https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
